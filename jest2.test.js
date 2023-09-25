2d99b1fb962f241c20c90414ea281e1ab9e20810
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-09-25 07:40:12 -0400

    Add second jest test

---
+++ jest2.test.js
@@ -0,0 +1,10 @@
+import * as tf from '@tensorflow/tfjs-node';
+
+
+describe('bar', () => {
+  it('bars', () => {
+    const t = tf.ones([1]);
+    expect(t.shape).toEqual([1]);
+    t.dispose();
+  });
+});
